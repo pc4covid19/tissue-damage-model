99d8ea54cead6006a5fa5f165890f2d196528998
Mohammad Aminul Islam <mixvc@mst.edu>
2020-10-02 16:35:41 -0500

    Updating Fibroblast recruitment

--- PhysiCell/custom_modules/immune_submodels.cpp
+++ PhysiCell/custom_modules/immune_submodels.cpp
@@ -507,6 +507,22 @@ void immune_cell_motility_direction( Cell* pCell, Phenotype& phenotype , double 
 	return; 
 }
 
+
+void fibroblast_motility_direction( Cell* pCell, Phenotype& phenotype , double dt )
+{
+	if( phenotype.death.dead == true )
+	{
+		phenotype.motility.migration_speed = 0.0; 
+		return;
+	}
+	static int antiinflammatory_cytokine_index = microenvironment.find_density_index("anti-inflammatory cytokine");
+	phenotype.motility.migration_bias_direction = pCell->nearest_gradient(antiinflammatory_cytokine_index);
+	normalize( &phenotype.motility.migration_bias_direction ); 
+	
+	return; 
+}
+
+
 void macrophage_phenotype( Cell* pCell, Phenotype& phenotype, double dt )
 {
 	static int apoptosis_index = phenotype.death.find_death_model_index( "Apoptosis" ); 
@@ -718,22 +734,20 @@ void neutrophil_mechanics( Cell* pCell, Phenotype& phenotype, double dt )
 	return; 
 }
 
-
-void fibroblast_phenotype( Cell* pCell, Phenotype& phenotype, double dt)
+//check11
+void fibroblast_phenotype( Cell* pCell, Phenotype& phenotype, double dt )
 {
 	
 	static int antiinflammatory_cytokine_index = microenvironment.find_density_index("anti-inflammatory cytokine");
 	static int collagen_index = microenvironment.find_density_index("collagen");
+	double TGF_beta = pCell->nearest_density_vector()[antiinflammatory_cytokine_index];
 
-
-    //std::vector<double> T_density = microenvironment.operator()(microenvironment.voxel_index)[antiinflammatory_cytokine_index]
-    //std::cout << T_density << " T_density ... " << std::endl;
-
+	//pCell->phenotype.secretion.secretion_rates[collagen_index] = (0.0092*pow(TGF_beta, 3) - 0.1552*pow(TGF_beta, 2) + 0.6279*TGF_beta + 0.2527)*(pCell->custom_data["collagen_secretion_rate"])*0.014;
 	pCell->phenotype.secretion.secretion_rates[collagen_index] = (pCell->custom_data["collagen_secretion_rate"])*0.014;
+	//pCell->phenotype.secretion.secretion_rates[collagen_index] = (0.05861064*pow(TGF_beta, 3) -1.25*pow(TGF_beta, 2) + 8.26504355*TGF_beta)*(pCell->custom_data["collagen_secretion_rate"])*0.014;
 
 
-			
-	
+	       
 	if( phenotype.death.dead == true )
 	{
 		pCell->functions.update_phenotype = NULL;
@@ -857,7 +871,7 @@ void immune_submodels_setup( void )
 	pCD->functions.custom_cell_rule = fibroblast_submodel_info.mechanics_function;
 	
     // needs to rewrite 
-	pCD->functions.update_migration_bias = immune_cell_motility_direction; 
+	pCD->functions.update_migration_bias = fibroblast_motility_direction; 
 	
 }
 
@@ -1058,7 +1072,7 @@ void immune_cell_recruitment( double dt )
 			{ create_infiltrating_neutrophil(); }
 		}
 
-
+        //check11
 	    //  fibroblast recruitment
 		static double fibroblast_recruitment_rate = parameters.doubles( "fibroblast_max_recruitment_rate" );
 		static double f_min_signal = parameters.doubles( "fibroblast_recruitment_min_signal" );
@@ -1071,7 +1085,11 @@ void immune_cell_recruitment( double dt )
 		for( int n=0; n<microenvironment.mesh.voxels.size(); n++ )
 		{
 			// (signal(x)-signal_min)/(signal_max/signal_min)
-			double dRate = ( microenvironment(n)[antiinflammatory_cytokine_index] - f_min_signal );
+			double TGF_beta = microenvironment(n)[antiinflammatory_cytokine_index];
+			//if( TGF_beta > 0 )
+			//{std::cout << TGF_beta << " TGF-beta ... " << n << "  signal  " << 0.0492*pow(TGF_beta,3) -0.9868*pow(TGF_beta,2) +6.5408*TGF_beta<<std::endl;}
+			double dRate = ( 0.0492*pow(TGF_beta,3) -0.9868*pow(TGF_beta,2) +6.5408*TGF_beta + 7 - f_min_signal );
+			//double dRate = (microenvironment(n)[antiinflammatory_cytokine_index] - f_min_signal );
 			dRate /= f_max_minus_min;
 			// crop to [0,1]
 			if( dRate > 1 )
